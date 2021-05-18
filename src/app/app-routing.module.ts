d3aa745e580c77be3735b5c36f16e4bcb06fff5b
Camilo <jcamilovelandiab@gmail.com>
2021-05-17 22:08:18 -0500

    Creating shared and country modules and Adding app-routing to the app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,43 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-const routes: Routes = [];
+import { ByCountryComponent } from './country/pages/by-country/by-country.component';
+import { ByRegionComponent } from './country/pages/by-region/by-region.component';
+import { ByCapitalComponent } from './country/pages/by-capital/by-capital.component';
+import { ViewCountryComponent } from './country/pages/view-country/view-country.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: ByCountryComponent,
+        pathMatch: 'full'
+    },
+    {
+        path: 'region',
+        component: ByRegionComponent
+    },
+    {
+        path: 'capital',
+        component: ByCapitalComponent
+    },
+    {
+        path: 'country/:code',
+        component: ViewCountryComponent
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [
+        RouterModule.forRoot(routes)
+    ],
+    exports: [
+        RouterModule
+    ]
+
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CountryModule } from './country/country.module';
+import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CountryModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
